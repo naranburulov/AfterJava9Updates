5a9510deffa04cce73b4260348d7837eef263c4b
naranburulov <naranburulov@gmail.com>
2022-08-23 09:37:54 -0400

    FactoryMethodsDemo class is completed

--- src/main/java/newVersionUpdates/java9/FactoryMethodsDemo.java
+++ src/main/java/newVersionUpdates/java9/FactoryMethodsDemo.java
@@ -1,10 +1,33 @@
 package newVersionUpdates.java9;
 
+import java.util.*;
+
 public class FactoryMethodsDemo {
 
     public static void main(String[] args) {
 
-        //Creating
+        //Creating unmodifiable List - BEFORE JAVA9
+        List<String> courses = Collections.unmodifiableList(Arrays.asList("Java", "Spring", "Agile"));
+        courses.add("TS");
+        System.out.println(courses);
+
+        //Creating unmodifiable List - AFTER JAVA9
+        List<String> myCourses = List.of("Java", "Spring", "Agile");
+
+        //Creating unmodifiable Set - AFTER JAVA9
+        Set<String> myProducts =Set.of("Milk", "Bread", "Butter");
+
+        //Creating unmodifiable Map - AFTER JAVA9
+        Map<String,Integer> myCart = Map.ofEntries(
+                Map.entry("Samsung TV", 1),
+                Map.entry("PSP",1),
+                Map.entry("Chair",5)
+        );
+
+
+
+
+
 
     }
 }
