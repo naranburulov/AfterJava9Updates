d8ba9392cbd47fa7ded0dbf7b7e90e92af71108c
naranburulov <naranburulov@gmail.com>
2022-08-23 21:56:49 -0400

    java9 module created, classes are redirected. java12 classes are completed

---
+++ java12/src/main/java/EventParticipation.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+class EventParticipation {
+
+
+  private List<String> guestNameList;
+  private Integer totalNumberOfParticipants;
+
+  public EventParticipation(List<String> guestNameList,
+   Integer totalNumberOfParticipants) {
+    this.guestNameList = guestNameList;
+    this.totalNumberOfParticipants = totalNumberOfParticipants;}
+
+  @Override
+  public String toString() {
+    return "EventParticipation{" +
+            "guestNameList=" + guestNameList +
+            ", totalNumberOfParticipants=" + totalNumberOfParticipants +
+            '}';
+  }
+}
--- src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module AfterJava9Updates {
-    // modularity is basically encapsulation of access to packages
-    // day49 video includes coding
-}
